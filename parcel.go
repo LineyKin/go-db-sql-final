7caf02858df68baa861516dbff5a95932c9662c2
Sam <yablokov-s-a@mail.ru>
2024-02-20 00:14:35 +0300

    метод добавления посылки

--- parcel.go
+++ parcel.go
@@ -13,10 +13,26 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 }
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
-	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
+	sqlPattern := "INSERT INTO parcel (client, status, address, created_at) VALUES(:client, :status, :address, :created_at)"
 
-	// верните идентификатор последней добавленной записи
-	return 0, nil
+	res, err := s.db.Exec(
+		sqlPattern,
+		sql.Named("client", p.Client),
+		sql.Named("status", p.Status),
+		sql.Named("address", p.Address),
+		sql.Named("created_at", p.CreatedAt))
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (s ParcelStore) Get(number int) (Parcel, error) {
